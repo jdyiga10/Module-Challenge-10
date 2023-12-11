bb37d565b65ed9ed459994ae370460d361f63d58
jdyiga10 <135932456+jdyiga10@users.noreply.github.com>
2023-12-11 13:12:48 +0300

    Add files via upload

--- Module_10_app.py
+++ Module_10_app.py
@@ -52,3 +52,42 @@ if __name__ == '__main__':
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/measurements<br/>"
+        f"/api/v1.0/stations"
+    )
+
+@app.route("/api/v1.0/measurements")
+def get_measurements():
+    """Return a list of measurements including date and prcp"""
+    # Query all measurements
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    # Convert the query results to a dictionary
+    measurements_data = []
+    for date, prcp in results:
+        measurement_dict = {}
+        measurement_dict["date"] = date
+        measurement_dict["prcp"] = prcp
+        measurements_data.append(measurement_dict)
+
+    return jsonify(measurements_data)
+
+@app.route("/api/v1.0/stations")
+def get_stations():
+    """Return a list of stations."""
+    # Query all stations
+    results = session.query(Station.station).all()
+
+    # Convert the query results to a list
+    stations_data = [station[0] for station in results]
+
+    return jsonify(stations_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
